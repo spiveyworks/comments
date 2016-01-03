bcacb7021b30716ab8533ee9ecf5a5bbeb684f97
Michael Spivey <michaelspivey@spiveyworks.com>
2016-01-02 21:27:10 -0500

    Added a new SpiveyWorks.Comments.Sql project for the SQL Server specific implementation of the comments repository. This implementation will accept a settings object of type Dictionary<string, object> and expect to receive a ConnectionString key value pair.

---
+++ src/SpiveyWorks.Comments.Sql/SqlCommentsRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpiveyWorks.Comments.Sql
+{
+    public class SqlCommentsRepository : ICommentsRepository
+    {
+        public static string ConnectionStringSettingName = "ConnectionString";
+
+        public string ConnectionString { get; set; }
+
+        public SqlCommentsRepository(Dictionary<string, object> settings)
+        {
+            if (settings[ConnectionStringSettingName] != null)
+                this.ConnectionString = (string)settings[ConnectionStringSettingName];
+        }
+
+        public void Create(Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(string commentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Comment[] GetByTargetId(string targetId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Comment comment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
